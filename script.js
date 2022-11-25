0e9bf25c885491e4cbec1c7a1db511af7d5e343a
Kingsley Ikpefan <itksweb@gmail.com>
2022-11-25 21:15:19 +0100

    updated to a more modern js standard

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-var color1 = document.getElementById('color1');
-var color2 = document.getElementById('color2');
-var css = document.querySelector('h3');
-var body = document.querySelector('body');
+let color1 = document.getElementById('color1');
+let color2 = document.getElementById('color2');
+let css = document.querySelector('h3');
+let body = document.querySelector('body');
 
-function setGradient(){
+const setGradient = () => {
 	body.style.background = 'linear-gradient(to right,'+ color1.value +', '+ color2.value+')';
 	css.textContent = body.style.background +';';
 }
-//color1.addEventListener('input', setGradient);
-//color2.addEventListener('input', setGradient);
-
 color1.oninput = setGradient;
 color2.oninput = setGradient;
